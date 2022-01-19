99f51544997db9a43dab015049000310d787d452
Magda Lipka <dev@magdalipka.com>
2022-01-19 13:53:54 +0100

    add function parsing postfix expression into evaluation tree

--- main.hs
+++ main.hs
@@ -1,22 +1,41 @@
 data TreeNode
-  = Const Bool
+  = Const Char
   | And TreeNode TreeNode
   | Or TreeNode TreeNode
   | Not TreeNode
   | Implication TreeNode TreeNode
   | Eq TreeNode TreeNode
 
-eval :: TreeNode -> Bool
-eval (Const a) = a
-eval (And a b) = eval a && eval b
-eval (Or a b) = eval a || eval b
-eval (Not a) = not (eval a)
-eval (Implication a b) = eval b || not (eval a) && not (eval b)
-eval (Eq a b) = eval a && eval b || not (eval a) && not (eval b)
+eval :: Bool -> Bool -> TreeNode -> Bool
+eval p q (Const 'p') = p
+eval p q (Const 'q') = q
+eval p q (And a b) = eval p q a && eval p q b
+eval p q (Or a b) = eval p q a || eval p q b
+eval p q (Not a) = not (eval p q a)
+eval p q (Implication a b) = eval p q a <= eval p q b
+eval p q (Eq a b) = eval p q a == eval p q b
+eval _ _ _ = error "Incorrect evaluation input"
+
+doubleTreeNode :: Char -> TreeNode -> TreeNode -> TreeNode
+doubleTreeNode '+' a b = Or a b
+doubleTreeNode '*' a b = And a b
+doubleTreeNode '>' a b = Implication a b
+doubleTreeNode '#' a b = Eq a b
+doubleTreeNode x _ _ = error "Incorrect doubleTreeNode input"
+
+singleTreeNode :: Char -> TreeNode -> TreeNode
+singleTreeNode '!' a = Not a
+singleTreeNode x _ = error "Incorrect singleTreeNode input"
+
+constTreeNode :: Char -> TreeNode
+constTreeNode = Const
 
 isUnaryOperator :: Char -> Bool
 isUnaryOperator '!' = True
 isUnaryOperator _ = False
+
+isDoubleOperator :: Char -> Bool
+isDoubleOperator x = isOperator x && not (isUnaryOperator x)
 
 isOperator :: Char -> Bool
 isOperator '*' = True
@@ -62,11 +81,19 @@ validateExpression (x : xs) Two margins
   | isOperator x = validateExpression xs Zero margins
   | otherwise = False
 
-buildTree :: String -> [String] -> TreeNode
-buildTree [] stack = Const True
-buildTree [x] stack = if x == '1' then Const True else Const False
+popOne :: [a] -> ([a], a)
+popOne x = (init x, last x)
+
+popTwo :: [a] -> ([a], a, a)
+popTwo x = (init (init x), last (init x), last x)
+
+buildTree :: String -> [TreeNode] -> TreeNode
+buildTree [] stack = last stack
 buildTree (x : xs) stack
-  | isOperator x = Const True
-  | isBracket x = Const True
-  | isOperand x = Const True
+  | isUnaryOperator x = buildTree xs (singlePoppedStack ++ [singleTreeNode x child])
+  | isDoubleOperator x = buildTree xs (doublePoppedStack ++ [doubleTreeNode x left right])
+  | isOperand x = buildTree xs (stack ++ [constTreeNode x])
   | otherwise = error "Incorrect character."
+  where
+    (doublePoppedStack, left, right) = popTwo stack
+    (singlePoppedStack, child) = popOne stack
